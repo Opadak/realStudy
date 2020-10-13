39b445f9ad436753418aa0e24fa639d6910b10ba
Opadak <tfk1019@naver.com>
2020-10-14 00:33:09 +0900

    2020-10-14 Commit

---
+++ app/src/main/java/com/example/myapplication/Fragment1.kt
@@ -0,0 +1,96 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment1.*
+
+
+//fragment 를 만들기
+
+class Fragment1:Fragment(){
+
+    interface OnDataPassListener {
+        fun onDataPass(data: String?)
+
+    }
+    lateinit var  dataPassListener : OnDataPassListener
+
+    override fun onAttach(context: Context) {
+        Log.d("Life_cycle","F onAttach")
+        super.onAttach(context)
+        dataPassListener = context as OnDataPassListener
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("Life_cycle","F onCreate")
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView( //view를 그리는 애 ****
+        inflater: LayoutInflater, // inflater > View 를 그려주는 애
+        container: ViewGroup?,    // container > 부모 View
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.d("Life_cycle","F onCreateView")
+        // 프라그먼트가 인터페이스를 처음으로 그릴때 호출된다.
+       return inflater.inflate(R.layout.fragment1,container,false)//root >>내가 붙을 뿌리
+        //원래 override하면 자동적으로 return이 생기는데 return은 view를 리턴을 함
+        //33줄이 view 그자체이기 떄문에 return을 앞에다 붙여주면 리턴값 완성임.
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.d("Life_cycle","F onViewCreated")
+        super.onViewCreated(view, savedInstanceState)
+
+        //Activity의 Oncreate에서 했던 작업을 여기에서 한다.
+        pass.setOnClickListener{
+            dataPassListener.onDataPass("Goodbye")
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        Log.d("Life_cycle","F onActivityCreated")
+
+        val data = arguments?.getString("hello")
+        Log.d("data",data)
+        //data를 log로 찍을려고 하는데 이 data가 null이다. >>nullpointException
+
+        super.onActivityCreated(savedInstanceState)
+
+    }
+
+    override fun onStart() {
+        Log.d("Life_cycle","F onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d("Life_cycle","F onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d("Life_cycle","F onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d("Life_cycle","F onStop")
+        super.onStop()
+    }
+
+    override fun onDestroyView() {
+        Log.d("Life_cycle","F onDestroyView")
+        super.onDestroyView()
+    }
+
+    override fun onDetach() {
+        Log.d("Life_cycle","F onDetach")
+        super.onDetach()
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/FragmentActivity.kt
@@ -0,0 +1,82 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.FragmentManager
+import kotlinx.android.synthetic.main.activity_fragment.*
+
+class FragmentActivity : AppCompatActivity(), Fragment1.OnDataPassListener {
+    override fun onDataPass(data: String?) {
+        Log.d("pass",""+data)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment)
+        Log.d("Life_cycle","oncreate")
+
+        val fragmentOne : Fragment1 = Fragment1()
+
+        //프라그먼트의 데이터를 넣어주는 방법
+        //bundle을 만들어줘서 프라그먼트에 끼어 넣어야 함.
+        val bundle : Bundle = Bundle()
+        bundle.putString("hello","hello")
+        fragmentOne.arguments = bundle
+
+        button.setOnClickListener{
+            //프라그먼트를 동적으로 작동하는 방법
+            //프라그먼트 붙이는 방법 replace/add
+            // val fragmentOne : Fragment1 = Fragment1()
+            val fragmentmanager: FragmentManager = supportFragmentManager // supportFragmentManager < 액티비티가 가지고있음
+
+            //Transaction
+            // 작업의 단위 -> 시작과 끝이 있다.
+            val fragmentTransaction = fragmentmanager.beginTransaction()
+            //시작하겠다.
+            fragmentTransaction.replace(R.id.container,fragmentOne)
+            //할일
+            fragmentTransaction.commit()
+            //commit >끝이다
+            //끝을내는 방법
+            //1. commit    > 시간 될 때 해 ! (이것을 사용하는게 좀 더 안정적이다.)
+            //2. commitnow > 지금 당장해! (과부화가 생길 수 가 있다.)
+
+
+        }
+        button2.setOnClickListener{
+           // val fragmentOne : Fragment1 = Fragment1() 24절의 구문과 다르다 < 지역함수 그래서 바깥으로 올려줘야함!
+            //프라그먼트 remove/detach 하는 방법
+            val fragmentManager = supportFragmentManager
+            val fragmentTrancation = fragmentManager.beginTransaction()
+            fragmentTrancation.remove(fragmentOne) //-> 떼고 붙이고 반복 가능
+            //fragmentTrancation.detach(fragmentOne) // ->반복 불가능 한번만 실행 가능
+            fragmentTrancation.commit()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Life_cycle","onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Life_cycle","onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Life_cycle","onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Life_cycle","onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Life_cycle","onDestroy")
+    }
+}
