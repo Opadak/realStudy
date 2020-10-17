4b116f8b33c5b8eef9bc20c5156ecbae476364e7
Opadak <tfk1019@naver.com>
2020-10-17 16:19:45 +0900

    2020-10-17 Commit

---
+++ app/src/main/java/com/example/myapplication/Context.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Context : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_context)
+
+
+        val context: Context = this //this라는 것은 Context의 자식 클래스이기 때문에 this넣어줘도 됨,
+
+
+        val applicationContext: Context = getApplicationContext() as Context
+
+    }
+}
