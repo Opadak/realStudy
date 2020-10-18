ed5173841ebfe1d9cbf7f1cb4b87c7fb83dfa83c
Opadak <tfk1019@naver.com>
2020-10-18 18:32:05 +0900

    2020-10-18 Commit

---
+++ app/src/main/java/com/example/myapplication/kotlin/AddViewActivity.kt
@@ -0,0 +1,41 @@
+package com.example.myapplication.kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.example.myapplication.R
+
+class AddViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_view)
+
+
+        //item list 준비
+        val carList = ArrayList<CarForList>()
+        for (i in 0 until 10) {
+            carList.add(CarForList("" + i + "번째 자동차", "" + i + "순위 엔진"))
+        }
+
+
+        val container = findViewById<LinearLayout>(R.id.addview_container)
+        val inflater = LayoutInflater.from(this)
+        for (i in 0 until carList.size) {
+            val itemView = inflater.inflate(R.layout.item_view, null)
+            val carnameView = itemView.findViewById<TextView>(R.id.car_name)
+            val carEnginView = itemView.findViewById<TextView>(R.id.car_engin)
+
+            carnameView.setText(carList.get(i).name)
+            carEnginView.setText(carList.get(i).engin)
+            container.addView(itemView)
+        }
+
+    }
+}
+
+class CarForList(val name: String, val engin: String) {
+
+
+}
