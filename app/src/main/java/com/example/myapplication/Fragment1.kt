1ed4f86185f4a4387e4465b7255157707576d90f
Opadak <tfk1019@naver.com>
2020-10-23 18:13:57 +0900

    2020-10-23 Commit

--- app/src/main/java/com/example/myapplication/Fragment1.kt
+++ app/src/main/java/com/example/myapplication/Fragment1.kt
@@ -34,13 +34,13 @@ class Fragment1:Fragment(){
     override fun onCreateView( //view를 그리는 애 ****
         inflater: LayoutInflater, // inflater > View 를 그려주는 애
         container: ViewGroup?,    // container > 부모 View
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle? //객체를 저장하는 용도
     ): View? {
         Log.d("Life_cycle","F onCreateView")
         // 프라그먼트가 인터페이스를 처음으로 그릴때 호출된다.
        return inflater.inflate(R.layout.fragment1,container,false)//root >>내가 붙을 뿌리
         //원래 override하면 자동적으로 return이 생기는데 return은 view를 리턴을 함
-        //33줄이 view 그자체이기 떄문에 return을 앞에다 붙여주면 리턴값 완성임.
+        //41줄이 view 그자체이기 떄문에 return을 앞에다 붙여주면 리턴값 완성임.
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
