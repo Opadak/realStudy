704814e4d4fffdc1f3f2385aa009b8bb06eb7d4f
Opadak <tfk1019@naver.com>
2020-10-28 23:55:47 +0900

    2020-10-28 Commit

--- app/src/main/java/com/example/myapplication/RecyclerViewActivity.kt
+++ app/src/main/java/com/example/myapplication/RecyclerViewActivity.kt
@@ -3,6 +3,10 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +26,29 @@ class RecyclerViewActivity : AppCompatActivity() {
 class Adapter(
     val itemlist: ArrayList<CarForList2>,
     val inflater: LayoutInflater
+) : RecyclerView.Adapter<Adapter.ViewHolder>() { //<>에 들어가는 것은 제너릭 타입
+    //RecyclerView.Adapter의 상속을 받는데 ViewHolder 타입이다.
+    //RecyclerView는
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val carName: TextView
+        val carEngine: TextView
 
+        init {
+            carName = itemView.findViewById(R.id.car_name)
+            carEngine = itemView.findViewById(R.id.car_engin)
+        }
 
-)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
